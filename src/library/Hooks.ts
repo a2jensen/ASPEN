8261c8dce6d74a30477369af29863a21a8ec7000
SabineLo <sabinelo2727@gmail.com>
2025-01-27 18:55:56 -0800

    merge

--- src/library/Hooks.ts
+++ src/library/Hooks.ts
@@ -7,16 +7,38 @@ import { CodeSnippet } from "./types";
 // read, get, update, patch, delete functions for the JSON file
 
 // pass in the data as a string, should pass in an object of CodeSnippet type
-export async function PostSnippet(){
+//export async function PostSnippet(){
 
-}
+//}
 
 // returns object of codesnippet type
-export async function GetSnippet(){
+//export async function GetSnippet(){
 
-}
+//}
 
 // pass in the data as a string, should pass in an object of CodeSnippet type
-export async function UpdateSnippet(){
+//export async function UpdateSnippet(){
 
+//}
+
+const snippetLibrary: CodeSnippet[] = [];
+
+export function addSnippet(name: string, code: string): void{
+    const newSnippet: CodeSnippet = {
+        id: new Date().getTime().toString(),
+        name,
+        code,
+    };
+    snippetLibrary.push(newSnippet);
+}
+
+export function getSnippets(): CodeSnippet[]{
+    return snippetLibrary;
+}
+
+export function removeSnippet(id: string): void{
+    const index = snippetLibrary.findIndex((snippet) => snippet.id === id );
+    if (index !== -1){
+        snippetLibrary.splice(index, 1);
+    }
 }
