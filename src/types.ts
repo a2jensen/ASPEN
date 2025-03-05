59fdf2b76e354721d451ded4a1a7a259d4e13ad3
a2jensen <a2jensen@ucsd.edu>
2025-03-05 13:46:57 -0800

    Refactors to Classes and Extension init

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,37 @@
-interface Template {
-    /** ID associated with the template - generated with DateNow() */
-    id: string;
-    /** Name associated with the template */
-    name: string;
-    /** Content/snippet associated with the template */
-    content: string;
 
-    /** Tracks date created as well as updated */
+/**
+ * @type id : string -  ID associated with the template, generated through`${DateNow()}`
+ * @type name : string - Name associated with the template
+ * @type content : string - Content/string associated with the template
+ * @type : dateCreated : Date - Tracks date creation of template
+ * @type : dateUpdated : Date - Tracks most recent updates for the template
+ * @type : tags : string[]  - Associates tags with templates - not implemented as of 3/5/25
+ * @type color : string  - Color associated with the template - not implemented as of 3/5/25
+ * @type : connections : string[] - Tracks associated snippet instances - not implemented as of 3/5/25 and may need refactoring
+ */
+export interface Template {
+    id: string;
+    name: string;
+    content: string;
     dateCreated: Date;
     dateUpdated: Date;
-
-    /** Feature not implemented yet - but will allow for certain tags to marked onto a tempalte */
     tags: string[];
-
-    /** Sets a color to a template, not in use */
     color: string;
-
-    /** Holds IDs of the snippet instances */
     connections: string[];
-  }
+}
 
-  export default Template;
+/**
+ * @type cell_id : number -  Unique identifier for the cell/view containing this snippet
+ * @type content : string - Content/string associated with the snippet
+ * @type start_line : number - starting line number in view/editor
+ * @type end_line : number - ending line number in view/editor
+ * @type template_id : string - reference to associated templateID
+ */
+export interface Snippet {
+  cell_id: number;
+  content : string;
+  start_line: number;
+  end_line: number;
+  template_id: string;
+}
+
